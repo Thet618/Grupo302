a9f408f040eaf686dd164abe682341620187f164
Thet618 <danielsummon258@gmail.com>
2025-12-10 09:57:16 -0600

    se creo la carpeta para que pongan sus archivos

---
+++ suma.py
@@ -0,0 +1,9 @@
+def sumar_tres_numeros(a, b, c):
+    return a + b + c
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+resultado = sumar_tres_numeros(num1, num2, num3)
+print("La suma de los tres números es:", resultado)
