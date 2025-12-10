12dbc1b4e0e93e2f0713fa3b1716273e45daa35f
RataCiega321 <suikahdz98@gmail.com>
2025-12-10 10:27:24 -0600

    Add files via upload

---
+++ multiplicacion.py
@@ -0,0 +1,9 @@
+def multiplicar():
+    nm1= float(input("ingresa el primer numero:"))
+    nm2= float(input("ingresa el segundo numero:"))
+    nm3= float(input("ingresa el tercer numero:"))
+    resultado=nm1 * nm2 * nm3
+    print ("la multiplicacion es:", resultado)
+
+if __name__=="__main__":
+    multiplicar()
